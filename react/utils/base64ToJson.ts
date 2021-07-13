9bc118b776370b44ea7e7157aee36881d131a173
arielabaruffaldi <baruffaldiariela@gmail.com>
2021-07-13 12:48:18 -0300

    fix in decoding base64 to json special characters

--- react/utils/base64ToJson.ts
+++ react/utils/base64ToJson.ts
@@ -1,14 +1,15 @@
 import atob from "atob";
 
+
 const base64ToJson = (file: string) => {
-
   const textToSplit = "base64,";
-
   let indexOfBase64 = file.indexOf(textToSplit);
   indexOfBase64 = indexOfBase64 + textToSplit.length;
 
   const stringToAtob = file.slice(indexOfBase64);
-  const json = atob(stringToAtob);
+  const json = decodeURIComponent(atob(stringToAtob).split('').map(function (c) {
+    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+  }).join(''))
   return JSON.parse(json)
 };
 
